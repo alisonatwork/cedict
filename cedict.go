ba235c6266ef75c7bfdeac6913de4647a1a44c91
Alison Winters <alisonatwork@outlook.com>
2019-08-04 11:33:34 +0800

    add support for downloading the dict

--- cedict.go
+++ cedict.go
@@ -1,19 +1,65 @@
 package main
 
 import (
+	"compress/gzip"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 
 	"github.com/hermanschaaf/cedict"
 )
 
-func openCeDict() (*os.File, error) {
+const cedictGzipUrl = "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz"
+
+func getLocalPath() (string, error) {
 	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return home + "/tmp/cedict_1_0_ts_utf-8_mdbg.txt", nil
+}
+
+func downloadCeDict() error {
+
+	resp, err := http.Get(cedictGzipUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	r, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	path, err := getLocalPath()
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	fmt.Printf("Downloading: %s to %s\n", cedictGzipUrl, path)
+
+	_, err = io.Copy(out, r)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func openCeDict() (*os.File, error) {
+	path, err := getLocalPath()
 	if err != nil {
 		return nil, err
 	}
-	path := home + "/tmp/cedict_ts.u8"
 
 	return os.Open(path)
 }
@@ -23,9 +69,17 @@ func main() {
 		return
 	}
 
+	if len(os.Args) == 2 && os.Args[1] == "get" {
+		err := downloadCeDict()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s!\n", err)
+		}
+		return
+	}
+
 	f, err := openCeDict()
 	if os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "Dictionary not found: getcedict first!\n")
+		fmt.Fprintf(os.Stderr, "Dictionary not found: get first!\n")
 		return
 	} else if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s!\n", err)
