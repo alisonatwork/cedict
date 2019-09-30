1de51080fd871b2da9d6fbcf86a266181a7fb593
Alison Winters <alisonatwork@outlook.com>
2019-10-01 01:22:17 +0800

    apply the 1 char = 1 word strategy to all arguments when -m is enabled

--- cedict.go
+++ cedict.go
@@ -10,6 +10,51 @@ import (
 	"github.com/alisonatwork/cedict/pinyin"
 )
 
+type matchStrategy int
+
+const (
+	exact matchStrategy = iota
+	splitChar
+)
+
+func printEntries(defs []*lookup.Entry) {
+	for _, e := range defs {
+		fmt.Printf("%s\t[%s]\t/%s/\n", e.Simplified, pinyin.NumberToMark(e.Pinyin), pinyin.NumberToMark(strings.Join(e.Definitions, "/")))
+	}
+}
+
+func output(strategy matchStrategy, lookup lookup.Lookup, word string) {
+	defs := lookup.Simplified[word]
+	if len(defs) == 0 {
+		defs = lookup.Traditional[word]
+	}
+	if len(defs) == 0 {
+		if len(word) > 1 {
+			switch strategy {
+			case exact:
+				break
+			case splitChar:
+				for _, c := range strings.Split(word, "") {
+					output(strategy, lookup, c)
+				}
+				return
+			}
+		}
+		// convert traditional to simplified before printing unknown char
+		for _, c := range strings.Split(word, "") {
+			defs = lookup.Traditional[c]
+			if len(defs) == 0 {
+				fmt.Printf("%s", c)
+			} else {
+				fmt.Printf("%s", defs[0].Simplified)
+			}
+		}
+		fmt.Printf("\n")
+	} else {
+		printEntries(defs)
+	}
+}
+
 func main() {
 	if len(os.Args) == 1 {
 		return
@@ -23,9 +68,11 @@ func main() {
 		return
 	}
 
+	var strategy = exact
 	var words []string
 	if len(os.Args) > 2 && os.Args[1] == "-m" {
-		words = append(strings.Split(os.Args[2], ""), os.Args[3:]...)
+		words = os.Args[2:]
+		strategy = splitChar
 	} else {
 		words = os.Args[1:]
 	}
@@ -52,25 +99,6 @@ func main() {
 	}
 
 	for _, word := range words {
-		defs := lookup.Simplified[word]
-		if len(defs) == 0 {
-			defs = lookup.Traditional[word]
-		}
-		if len(defs) == 0 {
-			// no exact match, let's still try to convert trad to simplified
-			for _, e := range strings.Split(word, "") {
-				defs = lookup.Traditional[e]
-				if len(defs) == 0 {
-					fmt.Printf("%s", e)
-				} else {
-					fmt.Printf("%s", defs[0].Simplified)
-				}
-			}
-			fmt.Printf("\n")
-		} else {
-			for _, e := range defs {
-				fmt.Printf("%s\t[%s]\t/%s/\n", e.Simplified, pinyin.NumberToMark(e.Pinyin), pinyin.NumberToMark(strings.Join(e.Definitions, "/")))
-			}
-		}
+		output(strategy, lookup, word)
 	}
 }
